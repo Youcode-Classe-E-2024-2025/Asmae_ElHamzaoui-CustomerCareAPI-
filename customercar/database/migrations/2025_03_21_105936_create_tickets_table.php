e327bc8c5fb685282ab5847dbc06991f3d2f003a
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-21 11:08:50 +0000

    Ajout des colonnes à la migration tickets

--- customercar/database/migrations/2025_03_21_105936_create_tickets_table.php
+++ customercar/database/migrations/2025_03_21_105936_create_tickets_table.php
@@ -9,10 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up() {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Client
+            $table->foreignId('agent_id')->nullable()->constrained('users')->onDelete('set null'); // Agent
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['ouvert', 'en cours', 'résolu', 'fermé'])->default('ouvert');
             $table->timestamps();
         });
     }
