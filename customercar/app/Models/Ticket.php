f8a25a5a340d869b29afe6be2bc2e3108e63f638
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-21 11:11:35 +0000

    Ajout des fillables ainsi que les relations entre les entités dans le model ticket

--- customercar/app/Models/Ticket.php
+++ customercar/app/Models/Ticket.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'agent_id', 'title', 'description', 'status'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function agent() {
+        return $this->belongsTo(User::class, 'agent_id');
+    }
 }
