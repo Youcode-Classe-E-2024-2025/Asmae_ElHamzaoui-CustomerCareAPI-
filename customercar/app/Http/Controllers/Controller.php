b54837a2579214dcdd94aaf66d4ebb54324d7863
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-22 14:43:18 +0000

    Ajout d annotation @OA\infos dans le controleur père

--- customercar/app/Http/Controllers/Controller.php
+++ customercar/app/Http/Controllers/Controller.php
@@ -6,6 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Customer Car",
+ *      description="Documentation for my Laravel API",
+ *      @OA\Contact(
+ *          email="your_email@example.com"
+ *      ),
+ *      @OA\License(
+ *          name="MIT",
+ *          url="https://opensource.org/licenses/MIT"
+ *      )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
